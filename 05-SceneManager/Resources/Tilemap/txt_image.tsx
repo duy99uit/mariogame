228b68ca7e32a26f19e8ba8a5927961ae6a7001c
duy99uit <58467999+duy99uit@users.noreply.github.com>
2022-05-20 23:09:13 +0700

    delete file

---
+++ 05-SceneManager/Resources/Tilemap/txt_image.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="txt_image" tilewidth="16" tileheight="16" tilecount="4293" columns="159">
+ <image source="world1-3_bank.png" width="2559" height="435"/>
+</tileset>
